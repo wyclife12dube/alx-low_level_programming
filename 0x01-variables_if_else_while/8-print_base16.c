1cbeca05dd0dde91f4dc2fbce441907df8dc959e
wyclife12dube <wyclife12dube@gmail.com>
2022-09-09 10:04:13 -0700

    Base 16 data

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry pint
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char num,
+	     letter,
+	     space = '\n';
+	for (num - '0'; num <= '9'; ++num)
+	{
+		putchar(num);
+	}
+	for (letter = 'a'; letter <= 'f'; ++letter)
+	{
+		putchar(letter);
+	}
+	putchar(space);
+	return (0);
+}
