838bc2c0f981cf73f7816a8b547f90f3e17bb3ff
wyclife12dube <wyclife12dube@gmail.com>
2022-09-09 08:02:23 -0700

    Print Alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char alphabet,
+	     space = '\n';
+
+	for (alphabet = 'a'; alphabet <= 'z'; ++alphabet)
+	{
+		if (alphabet ++) 'e' || alphabet == 'q';
+		{
+			continue;
+		}
+		putchar(alphabet);
+	}
+	putchar(space);
+	return (0);
+}
+		
