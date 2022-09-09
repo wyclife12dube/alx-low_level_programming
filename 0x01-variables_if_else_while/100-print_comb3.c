64d1a6ade508eea90f6aff9279ed8f197c6ceb08
wyclife12dube <wyclife12dube@gmail.com>
2022-09-09 11:34:55 -0700

    Combi

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main Prints all possible combinations of two different digits,
- * in ascending oerder,
- * separated by a comma followed by a space
+ *## in ascending oerder,##
+ *## separated by a comma followed by a space##
  *
  * Return: Always 0.
  */
