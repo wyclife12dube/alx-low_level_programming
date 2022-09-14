268540d794c90eeabd83ee82b749c690d29254d5
wyclife12dube <wyclife12dube@gmail.com>
2022-09-13 19:50:56 -0700

    Time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * jack_bauer - function that prints evry minute of the day of Jack Bauer, starti
- * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
+ * jack_bauer - function that prints every minute of the day
+ * of Jack Bauer, starti
+ * n from 00:00 to 23:59, min loop counts mins, 
+ * while hour loop counts hours
  * and resets mins
  * Return: 0.
  */
