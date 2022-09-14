0b6cb45fe1a423f64f846243219b67d8a96f77b4
wyclife12dube <wyclife12dube@gmail.com>
2022-09-13 20:07:02 -0700

    Table times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,12 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_puchcahr('0');
+		_putchar('0');
 
-		for (mult = 1; ,ult <= 9; mult++)
+		for (mult = 1; ,mult <= 9; mult++)
 		{
 			_putchar(',');
-			_putcahr(' ');
+			_putchar(' ');
 
 			prod = num * mult;
 
