3d2f985792131c1733c1b552944d5a1e796f77bc
wyclife12dube <wyclife12dube@gmail.com>
2022-09-13 17:30:20 -0700

    Returning 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbes
+ * ranging fom 0-99, separated by a comma followed by a space
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	printf("_putchar \n");
+	return 0;
+}
