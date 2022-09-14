496248f2cdf694eeadb564195101c51f3c86333d
wyclife12dube <wyclife12dube@gmail.com>
2022-09-14 07:31:14 -0700

    Alpha charactors

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _isalpha - cheching if letter are lowercase or uppercase
+ * _isaplha - checking if letters are lowercase or uppercase
  * @c: will use the int
  * Return: 0.
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	else
-		return (0);
+	if ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+			{
+			return (1);
+			}
+			else
+			return (0);
 }
