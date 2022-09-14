5fe37b76b15c9f7e8c4e9c7be2f3266ea0a7b3e9
wyclife12dube <wyclife12dube@gmail.com>
2022-09-13 19:28:44 -0700

    Printing the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,12 +5,13 @@
  *
  * Return: 0.
  */
-int print_last_digit
+int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
 	if (last_digit < 10)
-		last_digit *= -1;
+		last_digit *= -1
+			;
 	_putchar(last_digit + '0');
 
 	return (last_digit);
