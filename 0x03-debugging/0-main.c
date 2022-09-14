b8f8120af45e8a41281e3d9a91d874dae6922dff
wyclife12dube <wyclife12dube@gmail.com>
2022-09-14 05:22:01 -0700

    Negative or positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - tests the function that prints positive and negative
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
