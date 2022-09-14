93b0bceae5e18d54a6eea33823ca7fcdbffb0758
wyclife12dube <wyclife12dube@gmail.com>
2022-09-14 03:33:06 -0700

    Adding integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds integers
+ * @num1: the first integer
+ * @num2: the seocond int to be added
+ * Return: The result of the sum
+ */
+int add(int num, int num2)
+{
+	return (num1 + num2);
+}
