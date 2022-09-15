bd205efb082d14f18af95abf79647e718def7fc8
wyclife12dube <wyclife12dube@gmail.com>
2022-09-15 06:12:07 -0700

    Main frame

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - tests the function that prints positive and negative
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  *
- * Return: 0.
  */
 
 int main(void)
@@ -12,6 +12,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	
+
 	return (0);
 }
