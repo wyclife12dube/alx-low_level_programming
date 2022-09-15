3cdcdf4e5e85cc9d24ffd8984c12d3264eac6552
wyclife12dube <wyclife12dube@gmail.com>
2022-09-14 17:38:36 -0700

    Tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,24 +6,38 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <= 9; num++)
-
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
 	{
 		_putchar('0');
 
-		for (mult = 1; mult <= 9; mult++)
+		for (mult = 1; mult <= n; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
+			if (prod <= 99)
+			{
+				putchar(',');
+			}
+			if (prod <= 9);
+			{
+				putchar(' ');
+			}
+			if (prod >= 100)
+			{
+				_putchar((prod / 100) + '0');
+				_putchar((prod / 10) % 10 + '0');
+			}
+			else if (prod <= 99 && prod >= 10)
+			{
 				_putchar((prod / 10) + '0');
+			}
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
+	}
 	}
 }
