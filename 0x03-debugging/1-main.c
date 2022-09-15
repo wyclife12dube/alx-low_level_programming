24844e5cc772a164eb9d175fdb8515bc97f0e051
wyclife12dube <wyclife12dube@gmail.com>
2022-09-15 07:21:20 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,25 +3,25 @@
 /**
  * main - infinite loops
  * Return: 0
- */
+ * */
 
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
-	
+
 	/*
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
+	* while (i < 10)
+	* {
+	* putchar(i);
+	* }
+	* */
 
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
 
