5252c17d3cfbc2867864ad382469d9bba1a32764
wyclife12dube <wyclife12dube@gmail.com>
2022-09-19 13:00:21 -0700

    Printing arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,9 @@ void print_array(int *a, int n)
 	for (inc = 0; inc < n; inc++)
 	{
 		if (inc != n - 1)
-			printf("%d ", a[inc]);
+			printf("%d, ", a[inc]);
 		else
-			printf("%d ", a[inc]);
+			printf("%d", a[inc]);
 	}
-	_putchar(10);
+	putchar(10);
 }
