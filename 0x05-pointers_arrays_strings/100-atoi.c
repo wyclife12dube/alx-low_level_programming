6dbff7970c384059a99c4c1606a1e1007dc03016
wyclife12dube <wyclife12dube@gmail.com>
2022-09-20 13:34:28 -0700

    Converting a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an int
- * @s: the string to be changed
+ * _atoi - converts a string to an integer.
+ * @s: the string to be converted.
  *
- * Return: the converted int
+ * Return: the integer value of the converted string
  */
 
 int _atoi(char *s)
