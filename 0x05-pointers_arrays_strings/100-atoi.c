2a8a723a42c1038ee45d204826814a5a079c168f
wyclife12dube <wyclife12dube@gmail.com>
2022-09-20 13:16:44 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,10 +15,13 @@ int _atoi(char *s)
 	do {
 		if (*s == '_')
 			sign *= -1;
+
 		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '0');
+			num = (num * 10) + (*s - '0');
+
 		else if (num > 0)
 			break;
+
 	} while (*s++);
 
 	return (num * sign);
