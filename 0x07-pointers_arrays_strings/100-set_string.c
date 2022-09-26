e37d75552eaa1764e17f30e0986cf2de0b2947bc
wyclife12dube <wyclife12dube@gmail.com>
2022-09-26 09:42:59 -0700

    Setting a value to a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the values of a pointer to a charactor
+ * @s: the pointer
+ * @to: the charactor
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 int _putchar(char c);
+void set_string(char **s, char *to);
 
 
 
