794ef78a5e6340761d521db834c6480be35f6d7a
wyclife12dube <wyclife12dube@gmail.com>
2022-09-26 02:40:12 -0700

    Memory set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,16 +2,16 @@
 
 /**
  * _memset - function fills the first n bytes of the memory area pointed to
- * @s: s string 
+ * @s: a string
  * @b: charactor
  * @n: integer
  *
  * Return: a pointer to the memory area s
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	unassigned int a;
+	unsigned int a;
 
 	for (a = 0; a < n; a++)
 		s[a] = b;
