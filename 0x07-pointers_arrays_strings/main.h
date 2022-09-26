126cac5dd9f4652f5be6426d90c7adeadbcf7d65
wyclife12dube <wyclife12dube@gmail.com>
2022-09-26 07:57:54 -0700

    Printing chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
+int _putchar(char c);
 
 
 
