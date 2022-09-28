1358f05eedf1d774548b54abffd25c704c5363e4
wyclife12dube <wyclife12dube@gmail.com>
2022-09-28 07:35:13 -0700

    Puts recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the chasractor c to stdout
+ * @c: the charactor to print
+ *
+ * return: on success 1
+ * on error, -1 is returned and errorno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
