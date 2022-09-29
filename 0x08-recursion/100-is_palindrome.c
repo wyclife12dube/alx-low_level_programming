dc18a0c3b72390c8ff643505a6a5e02060b14922
wyclife12dube <wyclife12dube@gmail.com>
2022-09-29 14:16:56 -0700

    Wildcmp

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,6 +9,7 @@
 int is_palindrome(char *s)
 {
 	int flag = 1;
+	
 	check(s, 0, _strlen_recursion(s) - 1, &flag);
 	return (flag);
 }
@@ -44,6 +45,7 @@ void check(char *s, int start, int end, int *flag)
 int _strlen_recursion(char *s)
 {
 	int sum = 0;
+	
 	if (*s != '\0')
 	{
 		sum++;
