ccf6b03a97cd1571b306f8c1784b80697f1054cb
wyclife12dube <wyclife12dube@gmail.com>
2022-09-28 19:49:21 -0700

    Checking if the number is divisible

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,4 @@
 #include "main.h"
-int is_divisible(int num, int div);
-int is_prime_number(int n);
 
 /**
  * is_divisible - checks if a number is divisible
@@ -11,14 +9,12 @@ int is_prime_number(int n);
  * if the number is not divisible -1
  */
 
-int is_divisible(int num, int div)
+int is_prime_number(int n)
 {
-	if (num % div == 0)
+	int start = n / 2;
+
+	if (n <= 1)
 		return (0);
 
-	if (div == num / 2)
-		return (1);
-
-	return (is_divisible(num, div + 1));
-
+	return (is_prime(n, start));
 }
