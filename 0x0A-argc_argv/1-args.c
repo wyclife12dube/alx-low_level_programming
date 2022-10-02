7083ebd9ec2db1a75b33be4bb6fd981259be0f25
wyclife12dube <wyclife12dube@gmail.com>
2022-10-01 21:20:11 -0700

    Printing the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the programe
+ * @argc: arguments count
+ * @argv: arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
