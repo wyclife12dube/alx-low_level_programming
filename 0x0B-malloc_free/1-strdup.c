e01ba4b4ee46235040e3a9513145992ac9faf876
wyclife12dube <wyclife12dube@gmail.com>
2022-10-05 00:05:02 -0700

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,31 +1,69 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * _strdup - duplicates the new memory space location
- * @str: chharactor
- * Return: 0
+ * _strlen - count array
+ * @s: array of elements
+ * Return: Always 1
+ */
+
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copy arrays
+ * @src: array of elements
+ * @dest: destination array
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strdup - array for printing a string
+ * @str: array of elements
+ * Return: pointer
  */
 
 char *_strdup(char *str)
 {
-	char *array;
-	int i, r = 0;
+	char *dest;
+	unsigned int size;
 
-	if (str == NULL)
+	if (str == 0)
+	{
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
+	}
 
-	array = malloc(sizeof(char) * (i - 1));
+	size = _strlen(str) + 1;
+	dest = (char *) malloc(sizeof(char));
 
-	if (array == NULL)
+	if (dest == 0)
+	{
 		return (NULL);
-
-	for (r = 0; str[r]; r++)
-		array[r] = str[r];
-
-	return (array);
+	}
+	_strcpy(dest, str);
+	return (dest);
 }
