27b2c0076f83512482ffcc83039567dfdef4c521
wyclife12dube <wyclife12dube@gmail.com>
2022-10-14 03:54:21 -0700

    a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 	va_list valist;
 	unsigned int i = 0, j, c = 0;
 	char *str;
-	const char t_arg[] = "dub";
+	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
 	while (format && format[i])
@@ -29,16 +29,16 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
-			case 'd':
+			case 'c':
 				printf("%c", va_arg(valist, int)), c = 1;
 				break;
-			case 'u':
+			case 'i':
 				printf("%d", va_arg(valist, int)), c = 1;
 				break;
-			case 'b':
+			case 'f':
 				printf("%f", va_arg(valist, double)), c = 1;
 				break;
-			case 'e':
+			case 's':
 				str = va_arg(valist, char *), c = 1;
 				if (!str)
 				{
