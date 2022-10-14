51796e0064459de40f391b0f55431893f1c963d2
wyclife12dube <wyclife12dube@gmail.com>
2022-10-14 03:51:30 -0700

    a function that prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "dub";
+
+	va_start(valist, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (t_arg[j])
+		{
+			if (format[i] == t_arg[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+			case 'd':
+				printf("%c", va_arg(valist, int)), c = 1;
+				break;
+			case 'u':
+				printf("%d", va_arg(valist, int)), c = 1;
+				break;
+			case 'b':
+				printf("%f", va_arg(valist, double)), c = 1;
+				break;
+			case 'e':
+				str = va_arg(valist, char *), c = 1;
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+		} i++;
+	}
+	printf("\n"), va_end(valist);
+}
