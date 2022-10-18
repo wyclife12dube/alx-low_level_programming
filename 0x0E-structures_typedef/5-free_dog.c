8120d368f2da7bc8491210161350b0ca0fcfafd7
wyclife12dube <wyclife12dube@gmail.com>
2022-10-18 02:21:23 -0700

    a function that frees a given struct

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * free_dog - free dog
- * @d: dog to be freed
- * Return: void
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
  */
-
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free(d->owner);
-	free(d->name);
-	free(d);
+    if (d == NULL)
+        return;
+
+    free(d->owner);
+    free(d->name);
+    free(d);
 }
