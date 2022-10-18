424b5815bf2aa0864466857e3f22d710991f2ab2
wyclife12dube <wyclife12dube@gmail.com>
2022-10-18 01:41:31 -0700

    vi 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,48 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculates the sum of the two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculates the difference of two integers
+ * @a: first int
+ * @b: second int
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates the product of integers
+ * @a: first integer
+ * @b: second int
+ * Return: the difference of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - calculates the reminder of the dision of two integers
+ * @a: first int
+ * @b: second int
+ * Return: remainder of a devided b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
