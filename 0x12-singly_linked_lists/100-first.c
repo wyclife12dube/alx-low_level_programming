ed9f7343ac9d48daad21d088e23e2404d7e3b2b9
wyclife12dube <wyclife12dube@gmail.com>
2022-10-21 01:27:48 -0700

    a function that prints strings before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) hare(void);
-/**
- * hare - prints a string before the main function
- */
+void first(void) __attribute__ ((constructor));
 
-void hare(void)
+/**
+  * first - A function that prints first before main is executed
+  *
+  * Return: Nothing.
+  */
+void first(void)
 {
-	printf("You're best! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
